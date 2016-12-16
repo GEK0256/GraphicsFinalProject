c4cfee8f75e1771ce03f35bb12665cb9d7b9810b
GEK0256 <harona00@gmail.com>
2016-12-16 13:42:52 -0500

    Making the git

---
+++ app/src/main/java/arman/david/uro/jacob/haron/graphicsfinalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package arman.david.uro.jacob.haron.graphicsfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
